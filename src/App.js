7760a2667926bd9573be464f624e17dc64db0d3c
Isaj <ismaelsaad011@gmail.com>
2023-02-20 09:08:40 +0300

    Created doughnut chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'react-chartjs-2'
+import './App.css'
 
-function App() {
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+const App = () => {
+  const data = {
+    labels: ['Yes', 'No'],
+    datasets: [
+      {
+        label: 'Yes || No',
+        data: [50, 20],
+        backgroundColor: ['green', 'grey'],
+        borderColor: ['transparent'],
+        hoverBorderColor: ['transparent']
+      }
+    ]
+  }
+
+  const options = {
+    responsive: true,
+    cutout: '80%',
+    plugins: {
+      legend: {
+        display: false
+      },
+      tooltip: {
+        yAlign: 'bottom',
+        displayColors: false,
+        callbacks: {
+          label: function (tooltipItem, data, value) {
+            const score = tooltipItem.dataset.data
+            return `Risk Score: ${score}`
+          }
+        }
+      }
+    }
+  }
+  const optionsTwo = {
+    responsive: true,
+    cutout: '80%',
+    rotation: 225,
+    circumference: 270,
+    plugins: {
+      legend: {
+        display: false
+      },
+      tooltip: {
+        yAlign: 'bottom',
+        displayColors: false,
+        callbacks: {
+          label: function (tooltipItem, data, value) {
+            const score = tooltipItem.dataset.data
+            return `Risk Score: ${score}`
+          }
+        }
+      }
+    }
+  }
+
+  const textCenter = {
+    id: 'textCenter',
+    beforeDatasetsDraw(chart) {
+      const { ctx, data } = chart
+      const xCoor = chart.getDatasetMeta(0).data[0].x
+      const yCoor = chart.getDatasetMeta(0).data[0].y
+      const chartArea = chart.chartArea
+      ctx.save()
+
+      // Adjust font size based on chart width
+      const fontSize = Math.min(50, chartArea.width / 10)
+      const valueFontSize = Math.min(120, chartArea.width / 5)
+
+      ctx.font = `${fontSize}px mulish`
+      ctx.fillStyle = 'Black'
+      ctx.textAlign = 'center'
+      ctx.textBaseLine = 'middle'
+      ctx.fillText('RISK SCORE', xCoor, yCoor - fontSize * 0.5)
+
+      ctx.font = `${valueFontSize}px mulish`
+      ctx.fillStyle = 'Green'
+      ctx.fillText(data.datasets[0].data[0], xCoor, yCoor + valueFontSize * 0.6)
+      ctx.restore()
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div id='chart-wrapper'>
+          <Doughnut data={data} options={options} plugins={[textCenter]} />
+          <Doughnut data={data} options={optionsTwo} plugins={[textCenter]} />
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
